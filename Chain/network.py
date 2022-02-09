edfa244a96d0e6bc821437dae5c2a4abfc9271b5
RuofengX <weiruofeng@ruofengx.cn>
2022-02-09 16:38:42 +0800

    network

--- Chain/network.py
+++ Chain/network.py
@@ -2,7 +2,7 @@
 网络层
 分布式部署数据层实例，并通过共识算法同步和分发数据
 """
-
+import socket
 from Chain import data
 
 class Nodes():
@@ -12,14 +12,29 @@ class Nodes():
     
     def register(self, node):
         self.node_list.append()
+class BaseNode():
+    def __init__(self) -> None:
+        super().__init__(self)
 
-class Node():
+        self.address = []
+
+class Node(BaseNode):
     def __init__(self, nodes: Nodes) -> None:
         super().__init__(self)
 
         self.chain = data.BlockChain()
-        self.address = []
 
+    def get_ip_address(self, ipv6: bool=False):
+        if ipv6:
+            s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            s.connect(("2001:4860:4860:8888", 80))
+        else:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        self.address.append(ip)
+
+    # 利用MQTT的频道实现多实例通信
     def push(self):
         raise NotImplementedError
     
@@ -28,6 +43,23 @@ class Node():
     
     def pull(self):
         raise NotImplementedError
+
+class ManagerNode(BaseNode):
+    def __init__(self, ip_list):
+        super().__init__(self)
+
+        self.address = ip_list
+
+    def regist(self, node: dict)
+
+
+
+
+class LocalNode(Node):
+    def __init__(self):
+        super().__init__()
+
+        self.address.append()        
         
 #TODO: 接受数据、广播、接受广播
         
